65b322433cc5f76f29229ab46c8688d95f06666a
wj1990 <695724136@qq.com>
2016-11-12 22:45:21 +0800

    22:45

--- js/data.js
+++ js/data.js
@@ -4,6 +4,6 @@ var data =  [
                         {stName:"刘亦菲",gender:"女",age:22,height:171,weigth:60,_id:6},
                         {stName:"华仔",gender:"男",age:26,height:175,weigth:70,_id:1},
                         {stName:"王江",gender:"女",age:18,height:170,weigth:50,_id:3},
-                        {stName:"momo",gender:"女",age:38,height:166,weigth:50,_id:4},
+                        {stName:"薛之谦",gender:"女",age:38,height:166,weigth:50,_id:4},
                         {stName:"詹姆斯",gender:"男",age:30,height:175,weigth:65,_id:5}
                     ];
